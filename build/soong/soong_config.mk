4e7ec83c9ce63daeac31434b684d88c05cd73abd
Alessandro Astone <ales.astone@gmail.com>
2019-09-06 01:13:17 +0200

    vendor: soong: _contents -> _json_contents
    Change-Id: I2baa36bbb9aff3a7e70a293ed3352fb718eb611c

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,4 +1,4 @@
-_contents := $(_contents)    "caf":{$(newline)
+_json_contents := $(_json_contents)    "caf":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str,  Target_shim_libs,                      $(TARGET_LD_SHIM_LIBS))
@@ -15,6 +15,6 @@ $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECI
 $(call add_json_bool, Has_legacy_camera_hal1, $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
-_contents := $(_contents)__SV_END
+_json_contents := $(_json_contents)__SV_END
 
-_contents := $(_contents)    },$(newline)
+_json_contents := $(_json_contents)    },$(newline)
