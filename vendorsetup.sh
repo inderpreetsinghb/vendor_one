f4f2314448a9ef58ec97bc965f2a7d7ea678534e
Jake Weinstein <jake@aospa.co>
2019-09-13 06:38:51 +0200

    vendor: correct Snapdragon LLVM Compiler path
    Required for Android Q
    
    Change-Id: Ic554a00c2f8fc44a5b8946c913deb95c52bf5619
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 # SDClang Environment Variables
-export SDCLANG_AE_CONFIG=vendor/caf/sdclang/sdclangAE.json
-export SDCLANG_CONFIG=vendor/caf/sdclang/sdclang.json
+export SDCLANG_AE_CONFIG=$(pwd)/vendor/caf/sdclang/sdclangAE.json
+export SDCLANG_CONFIG=$(pwd)/vendor/caf/sdclang/sdclang.json
 export SDCLANG_SA_ENABLED=false
