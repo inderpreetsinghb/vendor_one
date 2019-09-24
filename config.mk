ae92b98f8ba680145b5f778d802d75e1a6c62717
inderpreetsinghb <inderpreetbhangu2@gmail.com>
2019-09-24 14:36:45 +0530

    mhh....

--- config.mk
+++ config.mk
@@ -1,3 +1,11 @@
 LOCAL_PATH := vendor/teamone/mod
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay/common/
+
+# Themed bootanimation
+TARGET_MISC_BLOCK_OFFSET ?= 0
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.misc.block.offset=$(TARGET_MISC_BLOCK_OFFSET)
+PRODUCT_PACKAGES += \
+    misc_writer_system \
+    themed_bootanimation
