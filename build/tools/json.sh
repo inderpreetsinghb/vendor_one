1bc6587ab34dff33104a603ad1aa442dd25e0689
inderpreetsinghb <inderpreetbhangu2@gmail.com>
2019-11-12 18:17:44 +0000

    vendor: add build info for OTA

---
+++ build/tools/json.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "{ \"response\": [ { \"datetime\": $(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2), \"filename\": \"$(basename $(ls $OUT/OneOS*.zip))\", \"id\": \"$((md5sum $OUT/OneOS*.zip) | cut -d ' ' -f1)\", \"size\": $(stat -c%s $OUT/OneOS*.zip), \"version\": \"$ONE_TYPE\"  }]}" | tee $OUT/update
+
