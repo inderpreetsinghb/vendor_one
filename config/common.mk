c428c505d0c2e9c5df83900896b499be1d49259a
inderpreetsinghb <inderpreetbhangu2@gmail.com>
2020-01-13 15:02:36 +0530

    add prebuilts

--- config/common.mk
+++ config/common.mk
@@ -6,20 +6,13 @@ DISABLE_EAP_PROXY := true
 # Bootanimation
 PRODUCT_PACKAGES += \
     bootanimation.zip
-
-# Build Snapdragon apps
+	
+# Custom off-mode charger
 PRODUCT_PACKAGES += \
-    Gallery \
-    SnapdragonMusic  \
-    CMFileManager \
-    messaging \
-    LiveWallpapersPicker
-
-# Build sound recorder
-PRODUCT_PACKAGES += SoundRecorder
-
-# Build WallpaperPicker
-PRODUCT_PACKAGES += WallpaperPicker
+    charger_res_images \
+    one_charger_res_images \
+    font_log.png \
+    libhealthd.one	
 
 ifneq ($(HOST_OS),linux)
 ifneq ($(sdclang_already_warned),true)
@@ -34,6 +27,7 @@ include vendor/one/sdclang/sdclang.mk
 endif
 
 include vendor/one/config/prebuilt.mk
+include vendor/one/config/packages.mk
 include vendor/one/config/branding.mk
 include vendor/one/config/prop.mk
 include vendor/one/config/gapps.mk
---
+++ config/packages.mk
@@ -0,0 +1,15 @@
+# Build OneOS apps
+PRODUCT_PACKAGES += \
+    Aurora \
+    Music \
+    SnapdragonGallery \
+    messaging \
+    Calender \
+    Browser \
+    ThemePicker \
+    FMRadio \
+    Updater \
+    OnePapers
+
+# Build sound recorder
+PRODUCT_PACKAGES += Recorder	
