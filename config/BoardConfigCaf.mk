d3f3919625553df1699f2adb9281a5c1bbfa780b
IPSingh <inderpreetbhangu2@gmail.com>
2020-02-29 20:13:05 +0530

    Revert "one : Introduce [1/2] Pathmaps"
    This reverts commit 68e4aa572d8f640f33098b8d37be6f3b6c1db2d3.

--- config/BoardConfigCaf.mk
+++ config/BoardConfigCaf.mk
@@ -3,6 +3,3 @@ include vendor/one/config/BoardConfigKernel.mk
 
 # Soong
 include vendor/one/config/BoardConfigSoong.mk
-
-# Qcom
-include vendor/one/config/BoardConfigQcom.mk
--- config/common.mk
+++ config/common.mk
@@ -25,6 +25,7 @@ else
 # include definitions for SDCLANG
 include vendor/one/sdclang/sdclang.mk
 endif
+
 include vendor/one/config/prebuilt.mk
 include vendor/one/config/packages.mk
 include vendor/one/config/branding.mk
