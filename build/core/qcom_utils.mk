821d8e0adc8e8aae4732a7f45b106f4081e8abe9
androiabledroid <mohd.faraz.abc@mail.com>
2020-03-03 14:09:47 +0530

    vendor: one: update configs
     * from https://github.com/LineageOS/android_vendor_lineage/commit/05aded86f3de6daf79c53940a8ccb0df7a43d3c8
    
    Signed-off-by: IPSingh <inderpreetbhangu2@gmail.com>

--- build/core/qcom_utils.mk
+++ build/core/qcom_utils.mk
@@ -1,5 +1,11 @@
 # Board platforms lists to be used for
 # TARGET_BOARD_PLATFORM specific featurization
+
+# Platform name variables - used in makefiles everywhere
+MSMNILE := msmnile #SM8150
+MSMSTEPPE := sm6150
+TRINKET := trinket #SM6125
+
 QCOM_BOARD_PLATFORMS := msm7x27
 QCOM_BOARD_PLATFORMS += msm7x27a
 QCOM_BOARD_PLATFORMS += msm7x30
@@ -21,7 +27,12 @@ QCOM_BOARD_PLATFORMS += msm_bronze
 QCOM_BOARD_PLATFORMS += apq8084
 
 QCOM_BOARD_PLATFORMS += sdm660
+QCOM_BOARD_PLATFORMS += sdm710
 QCOM_BOARD_PLATFORMS += sdm845
+
+QCOM_BOARD_PLATFORMS += $(TRINKET)
+QCOM_BOARD_PLATFORMS += $(MSMSTEPPE)
+QCOM_BOARD_PLATFORMS += $(MSMNILE)
 
 # MSM7000 Family
 
--- config/BoardConfigOne.mk
+++ config/BoardConfigOne.mk
@@ -1,5 +1,7 @@
 # Kernel
 include vendor/one/config/BoardConfigKernel.mk
 
+include vendor/one/build/core/BoardConfigQcom.mk
+
 # Soong
 include vendor/one/config/BoardConfigSoong.mk
